e59a81e6b4855fb76a141dc2520b21214ab5a511
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-06-10 00:05:15 +0200

    Improved SignInActivity

--- app/src/main/java/signin/SignInActivity.kt
+++ app/src/main/java/signin/SignInActivity.kt
@@ -1,10 +1,18 @@
 package signin
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.artfelt.artfelt.R
 import kotlinx.android.synthetic.main.activity_signin.*
+import kotlinx.android.synthetic.main.activity_signin.button_signup
+import kotlinx.android.synthetic.main.activity_signin.editText_email
+import kotlinx.android.synthetic.main.activity_signin.editText_password
+import kotlinx.android.synthetic.main.activity_signin.textView_signin
+import kotlinx.android.synthetic.main.activity_signin.textView_signup
+import kotlinx.android.synthetic.main.activity_signup.*
 import signup.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
@@ -46,13 +54,66 @@ class SignInActivity : AppCompatActivity() {
     }
 
     private fun initSignInButton() {
-        button_signin.text = getString(R.string.ACTION_SIGN_IN)
+        initSignInTextView()
+        hideSignInProgressBar()
+        manageOnClickSignInButton()
     }
 
     private fun initSignUpButton() {
-        button_signup.text = getString(R.string.ACTION_SIGN_UP)
+        initSignUpTextView()
         manageOnClickSignUpButton()
     }
+
+    private fun initSignInTextView() {
+        textView_signin.text = getString(R.string.ACTION_SIGN_IN)
+        textView_signin.textSize = 16f
+        textView_signin.isAllCaps = true
+        showSignInTextView()
+
+    }
+
+    private fun initSignUpTextView(){
+        textView_signup.text = getString(R.string.ACTION_SIGN_UP)
+        textView_signup.textSize = 16f
+        textView_signup.isAllCaps = true
+    }
+
+
+    private fun showLoadingSignInButton(){
+        //textView_signin.text = getString(R.string.LABEL_CONNECTION_IN_PROGRESS)
+        showSignInProgressBar()
+        hideSignInTextView()
+    }
+
+    private fun showSignInProgressBar() {
+        progressBar_signin.visibility = View.VISIBLE
+    }
+
+    private fun hideSignInProgressBar() {
+        progressBar_signin.visibility = View.GONE
+    }
+
+    private fun showSignInTextView() {
+        textView_signin.visibility = View.VISIBLE
+    }
+
+    private fun hideSignInTextView() {
+        textView_signin.visibility = View.GONE
+    }
+
+
+
+    private fun manageOnClickSignInButton() {
+        button_signin.setOnClickListener {
+
+            if(editText_email.text.toString().isEmpty()) {
+                editText_email.error = "Ceci est un test d'erreur"
+            } else {
+                showLoadingSignInButton()
+            }
+        }
+    }
+
 
     private fun manageOnClickSignUpButton() {
         button_signup.setOnClickListener {
