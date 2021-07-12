a2e75355514076a5ba6284c8ebc00f251600bb4b
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-07-12 21:22:12 +0200

    Added header view

---
+++ app/src/main/java/partials/HeaderView.kt
@@ -0,0 +1,57 @@
+package partials
+
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import api.models.UserBody
+import com.artfelt.artfelt.R
+import kotlinx.android.synthetic.main.view_header.view.*
+
+class HeaderView(
+    private val context: Context,
+    parent: ViewGroup,
+    user: UserBody,
+){
+
+    private val view =
+        LayoutInflater.from(context).inflate(R.layout.view_header, parent, false)
+
+    var mHeaderProfilePic = view.header_profile_pic
+    var mHeaderTitle = view.header_title
+    var mHeaderShopcart = view.header_shopcart
+
+    init {
+        parent.removeAllViews()
+        parent.addView(view)
+        initView(user)
+    }
+
+    private fun initView(user: UserBody) {
+        initHeaderProfilePic(user)
+        initHeaderTitle()
+        initHeaderShopcart()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun initHeaderProfilePic(user: UserBody) {
+        mHeaderProfilePic.clipToOutline = true
+        //if (user.image !== "") {
+            //mHeaderProfilePic.setImageResource(user.image)
+        //} else {
+            mHeaderProfilePic.setImageResource(R.drawable.image_default_profilepic)
+        // }
+    }
+
+    private fun initHeaderTitle() {
+        mHeaderTitle.text = context.getString(R.string.TITLE_APP_NAME)
+        mHeaderTitle.textSize = 18f
+    }
+
+    private fun initHeaderShopcart() {
+
+    }
+
+}
