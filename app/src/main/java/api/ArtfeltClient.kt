ddda6716ea8337a647c7250ec38a757dd3b6de2a
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-07-12 21:19:44 +0200

    Update environments

--- app/src/main/java/api/ArtfeltClient.kt
+++ app/src/main/java/api/ArtfeltClient.kt
@@ -1,6 +1,7 @@
 package api
 
 import api.interfaces.ArtfeltApiService
+import com.artfelt.artfelt.BuildConfig
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
@@ -8,8 +9,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ArtfeltClient {
-    private const val BASE_URL: String = "http://178.62.93.242:7701"
-
     private val gson : Gson by lazy {
         GsonBuilder().setLenient().create()
     }
@@ -20,7 +19,7 @@ object ArtfeltClient {
 
     private val retrofit : Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
