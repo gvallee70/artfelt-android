bd70b55e328f26229290d41b40c35f8e7d2867e8
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-07-12 21:21:37 +0200

    Added getUserInfos api call

--- app/src/main/java/api/interfaces/ArtfeltApiService.kt
+++ app/src/main/java/api/interfaces/ArtfeltApiService.kt
@@ -1,20 +1,22 @@
 package api.interfaces
 
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import api.models.User
+import api.models.UserBody
+import retrofit2.http.*
 
 interface ArtfeltApiService {
 
     @Headers("Content-Type: application/json")
     @POST("/v1/auth/sign-up")
-    suspend fun signUp(@Body user: User): Response<User>
+    suspend fun signUp(@Body userBody: UserBody): Response<UserBody>
 
     @Headers("Content-Type: application/json")
     @POST("/v1/auth/sign-in")
-    suspend fun signIn(@Body user: User): Response<User>
+    suspend fun signIn(@Body userBody: UserBody): Response<UserBody>
+
+    @Headers( "Content-Type: application/json")
+    @GET("/v1/user/info")
+    suspend fun getSelfInfos(@Header("token") token: String): Response<UserBody>
+
 
 }
--- app/src/main/java/api/models/User.kt
+++ app/src/main/java/api/models/User.kt
@@ -1,17 +1,5 @@
 package api.models
 
-import com.google.gson.annotations.SerializedName
-
-data class User(
-        @SerializedName("id") val id: Int? = null,
-        @SerializedName("first_name") val firstName: String? = null,
-        @SerializedName("last_name") val lastName: String? = null,
-        @SerializedName("address_street") val street: String? = null,
-        @SerializedName("address_zip_code") val zipCode: String? = null,
-        @SerializedName("address_city") val city: String? = null,
-        @SerializedName("username") val username: String? = null,
-        @SerializedName("mail") val email: String? = null,
-        @SerializedName("password") val password: String? = null,
-        @SerializedName("role") val role: String? = "CUSTOMER"
-
-)
+object User {
+    var infos: UserBody? = null
+}
